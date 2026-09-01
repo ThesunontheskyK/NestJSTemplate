483e63f3dd022e9743de50625b01c32d0f87e012
Narin Mungmesuk <122215505+ThesunontheskyK@users.noreply.github.com>
2026-09-01 16:37:48 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './middleware/errorHandler';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new GlobalExceptionFilter());
+  app.use(helmet());
   await app.listen(process.env.SERVER_PORT ?? 3000);
 }
 bootstrap();
